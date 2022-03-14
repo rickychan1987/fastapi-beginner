63ea495b74fde6dc2d98ff2706b4387fd2e90d0f
rickychan1987 <khaivanla1987@gmail.com>
2022-03-14 20:17:57 +0700

    Relationships

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,17 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,  ForeignKey
 from .database import Base
+from sqlalchemy.orm import relationship
 
 
 class Blog(Base):
     __tablename__ = 'blogs'
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    creator = relationship("User", back_populates="blogs")
 
 
 class User(Base):
@@ -16,3 +21,5 @@ class User(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
+
+    blogs = relationship("Blog", back_populates="creator")
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,15 +1,13 @@
+from typing import List
 from pydantic import BaseModel
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
-
+class Blog(BlogBase):
     class Config():
         orm_mode = True
 
@@ -18,3 +16,21 @@ class User(BaseModel):
     name: str
     email: str
     password: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
+
+    class Config():
+        orm_mode = True
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+
+    class Config():
+        orm_mode = True
