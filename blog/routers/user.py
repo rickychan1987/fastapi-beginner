cd8ab3cb4c61a1cb19b2202874372020c7f1f724
rickychan1987 <khaivanla1987@gmail.com>
2022-03-17 19:54:00 +0700

    Login and Verify Pass

---
+++ blog/routers/user.py
@@ -0,0 +1,22 @@
+from typing import List
+from fastapi import APIRouter, Depends, status, HTTPException
+from .. import schemas, models, database
+from sqlalchemy.orm import Session
+from ..repository import user
+
+router = APIRouter(
+    prefix="/user",
+    tags=['User']
+)
+
+get_db = database.get_db
+
+
+@router.post('/', response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    return user.create(request, db)
+
+
+@router.get('/{id}', response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return user.show(id, db)
