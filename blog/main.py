c7773f5503aca40fab75a574232ef2f9be731ecc
rickychan1987 <khaivanla1987@gmail.com>
2022-03-19 12:52:34 +0700

    Done

--- blog/main.py
+++ blog/main.py
@@ -1,14 +1,17 @@
 from fastapi import Depends, FastAPI, status, Response, HTTPException
+
+from blog.routers import authentication
 from . import schemas, models
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 from typing import List
-from .routers import blog, user
+from .routers import blog, user, authentication
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
